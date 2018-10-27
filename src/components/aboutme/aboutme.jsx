6ecb6e92e72fddce35cb5ec646efee4d208d786b
Tainá <tainaraiodeluz@gmail.com>
2018-10-26 23:51:10 -0300

    Fixed banner and about-me

--- src/components/aboutme/aboutme.jsx
+++ src/components/aboutme/aboutme.jsx
@@ -1,26 +1,32 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-materialize';
 import imgUser from '../../images/me.jpg';
+import { Parallax } from 'react-scroll-parallax';
 
 const AboutMe = () => (
-    <Container>
-        <Row>
-            <Col s={12}>
-                <div className="box-photo-me">
-                    <img src={imgUser} className="responsive-img" />
-                    <div className="box-text-me">
-                        <h3>
-                            Tainá
+    <Parallax
+        offsetYMin={-0}
+        offsetYMax={0}
+        className='parallax-about'
+    >
+        <Container>
+            <Row>
+                <Col s={12}>
+                    <div className="box-photo-me">
+                        <img src={imgUser} className="responsive-img" />
+                        <div className="box-text-me">
+                            <h3>
+                                Tainá
                         </h3>
-                        <p> Front End Developer<br />
-                            Amante dos animais e natureza. Desenvolvedora por profissão e fotógrafa iniciante por hobby ♡
+                            <p> Front End Developer<br />
+                                Amante dos animais e natureza. Desenvolvedora por profissão e fotógrafa iniciante por hobby ♡
                         </p>
+                        </div>
                     </div>
-                </div>
-            </Col>
-        </Row>
-
-    </Container>
+                </Col>
+            </Row>
+        </Container>
+    </Parallax>
 );
 
 export default AboutMe;
---
+++ src/components/album/album.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-materialize';
+
+
+const Album = () => {
+    <Container>
+        <Row>
+            <Col s={12}>
+
+            </Col>
+        </Row>
+    </Container>
+}
+
+export default Album;
